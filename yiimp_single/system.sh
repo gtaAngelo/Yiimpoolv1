316c4a73ca6702b8ab62ec1b55656831e459e585
gtaAngelo <gta@getablocks.com>
2025-11-13 19:10:32 +0100

    Update YiiMP repository clone URL

--- yiimp_single/system.sh
+++ yiimp_single/system.sh
@@ -217,7 +217,7 @@ sudo update-alternatives --set php /usr/bin/php8.1
 print_success "PHP version set to 8.1"
 
 print_header "Cloning YiiMP Repository"
-hide_output sudo git clone -b dev https://github.com/gtaAngelo/yiimp.git $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+hide_output sudo git clone -b dev https://github.com/afiniel/yiimp.git $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 
 print_success "YiiMP repository cloned successfully"
 
