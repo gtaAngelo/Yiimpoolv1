fd0ce271cd48c07871c0c1903f8003c77671916f
gtaAngelo <gta@getablocks.com>
2026-03-30 16:07:09 +0200

    Add new SQL files for parent coin ID

--- yiimp_single/db.sh
+++ yiimp_single/db.sh
@@ -140,6 +140,8 @@ SQL_FILES=(
     2025-10-28-add_algo_phihash.sql
     2025-11-19-add_algo_meowpow.sql
     2025-12-16-add_algo_soterg.sql
+    2026-03-30-add_parent_coin_id.sgl
+    2026-03-30-add_parent_coin_id_yiimp2.sgl
     2026-03-29-add_algo_hoohash_pepew.sql
 )
 
